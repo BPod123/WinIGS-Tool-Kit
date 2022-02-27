09b5f5b9c3c42689d9ecfbabf4ef70a4bc1970af
Rahul Komatineni <rkomatineni6@gatech.edu>
2022-02-26 20:36:14 -0500

    testing svm with diff kernels

---
+++ Anomaly Detection/Supervised_Traditional_ML/svmDiffKernels.py
@@ -0,0 +1,44 @@
+from sklearn import datasets
+
+from sklearn.model_selection import train_test_split
+
+from sklearn import svm
+
+from sklearn import metrics
+
+import matplotlib.pyplot as plt
+
+
+
+cancer = datasets.load_breast_cancer()
+
+#print("Features: ", cancer.feature_names)
+
+print("Labels: ", cancer.target_names)
+
+print(cancer.data.shape)
+
+#print(cancer.target)
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
+
+kernelTypes = ["linear", "poly", "rbf", "sigmoid"]
+
+for x in kernelTypes:
+    print("\n" + x)
+    clf = svm.SVC(kernel= x) 
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+    print("Precision:",metrics.precision_score(y_test, y_pred))
+
+    print("Recall:",metrics.recall_score(y_test, y_pred))
+    
+X = cancer.data[:, :2] # we only take the first two features. We could
+y = cancer.target    
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+    
